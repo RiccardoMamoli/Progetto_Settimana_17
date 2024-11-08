4b3d9066c726ba5c2040659b74f40bb581db4a18
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-11-08 14:42:08 +0100

    controller and service creation

--- gestione_eventi_final/src/main/java/riccardomamoli/gestione_eventi_final/repositories/EventoRepository.java
+++ gestione_eventi_final/src/main/java/riccardomamoli/gestione_eventi_final/repositories/EventoRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import riccardomamoli.gestione_eventi_final.entities.Evento;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
     List<Evento> findByLuogoEvento(String luogoEvento);
+    boolean exsistsByLuogoEventoAndDataEvento(String luogoEvento, LocalDate dataEvento);
 }
