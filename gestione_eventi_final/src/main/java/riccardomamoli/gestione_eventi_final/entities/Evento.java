ebdd7800376d3c4476dad70d05b819ad753add3e
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-11-08 11:57:22 +0100

    Update Evento.java

--- gestione_eventi_final/src/main/java/riccardomamoli/gestione_eventi_final/entities/Evento.java
+++ gestione_eventi_final/src/main/java/riccardomamoli/gestione_eventi_final/entities/Evento.java
@@ -24,6 +24,79 @@ public class Evento {
     @OneToMany(mappedBy = "evento", cascade = CascadeType.ALL)
     private List<Partecipazione> partecipazioni;
 
+    public Evento(){}
+
+    public Evento(String nomeEvento, String descrizioneEvento, LocalDateTime dataEvento, String luogoEvento, int postiDisponibili) {
+        this.nomeEvento = nomeEvento;
+        this.descrizioneEvento = descrizioneEvento;
+        this.dataEvento = dataEvento;
+        this.luogoEvento = luogoEvento;
+        this.postiDisponibili = postiDisponibili;
+    }
+
+    public Long getIdEvento() {
+        return idEvento;
+    }
 
 
+    public String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public void setNomeEvento(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public String getDescrizioneEvento() {
+        return descrizioneEvento;
+    }
+
+    public void setDescrizioneEvento(String descrizioneEvento) {
+        this.descrizioneEvento = descrizioneEvento;
+    }
+
+    public LocalDateTime getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDateTime dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getLuogoEvento() {
+        return luogoEvento;
+    }
+
+    public void setLuogoEvento(String luogoEvento) {
+        this.luogoEvento = luogoEvento;
+    }
+
+    public int getPostiDisponibili() {
+        return postiDisponibili;
+    }
+
+    public void setPostiDisponibili(int postiDisponibili) {
+        this.postiDisponibili = postiDisponibili;
+    }
+
+    public List<Partecipazione> getPartecipazioni() {
+        return partecipazioni;
+    }
+
+    public void setPartecipazioni(List<Partecipazione> partecipazioni) {
+        this.partecipazioni = partecipazioni;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "idEvento=" + idEvento +
+                ", nomeEvento='" + nomeEvento + '\'' +
+                ", descrizioneEvento='" + descrizioneEvento + '\'' +
+                ", dataEvento=" + dataEvento +
+                ", luogoEvento='" + luogoEvento + '\'' +
+                ", postiDisponibili=" + postiDisponibili +
+                ", partecipazioni=" + partecipazioni +
+                '}';
+    }
 }
